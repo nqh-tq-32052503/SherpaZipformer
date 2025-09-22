244e314bd9ba705ca8752bdf2dc225358bf8e38a
Nguyen Quoc Hieu <nguyenquochieu3205xt@gmail.com>
2025-09-22 11:49:29 +0700

    Upload evaluation metric

---
+++ evaluate.py
@@ -0,0 +1,37 @@
+# pip install -q jiwer==4.0.0
+
+import jiwer
+
+transformation = jiwer.Compose([
+    jiwer.RemovePunctuation(),
+    jiwer.RemoveMultipleSpaces(),
+    jiwer.ToLowerCase(),
+    jiwer.RemoveKaldiNonWords(),
+    jiwer.Strip(),
+    jiwer.ReduceToListOfListOfWords(),
+])
+
+def compute_wer(refs, hyps):
+    """
+    Compute WER given references and hypotheses.
+
+    Args:
+        refs: A list of reference texts.
+        hyps: A list of hypothesis texts.  
+    Returns:
+        wer: Word Error Rate (WER) score.
+    """
+    assert len(refs) == len(hyps), f"len(refs)={len(refs)} != len(hyps)={len(hyps)}"
+    all_wer = []
+    for pred, gt in zip(hyps, refs):
+        t_pred = pred.replace("▁", " ").lower()
+        wer_score = jiwer.wer(
+            gt,
+            t_pred,
+            reference_transform=transformation,
+            hypothesis_transform=transformation
+        )
+
+        all_wer.append(wer_score)
+    wer = sum(all_wer) / len(all_wer)
+    return wer
